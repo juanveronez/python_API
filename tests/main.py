f6bda57548f43c11016b416aaa28dc629cd8e174
Juan Veronez <juan.monteirov@gmail.com>
2025-02-05 10:36:35 -0300

    Add Pytest and httpx to create first api test

---
+++ tests/main.py
@@ -0,0 +1,24 @@
+from fastapi.responses import Response
+
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+def test_hello_world():
+    """
+    Should exists the / request and return the default message when request.
+    """
+
+    response: Response = client.get('/')
+
+    assert response.status_code == 200
+    assert response.json() == { "message": "hello world!" }
+
+def test_docs():
+    """""
+    should exists project doc as default, created by Fastapi.
+    """
+    response: Response = client.get('/docs')
+    assert response.status_code == 200
