212f33862e71f2cda48951e55d2a7eed119d519f
Juan Veronez <juan.monteirov@gmail.com>
2025-02-05 14:22:50 -0300

    update main file, create create product endpoint and update tests. Use shcema to validate request and responses

--- app/main.py
+++ app/main.py
@@ -1,62 +1,30 @@
-from typing import Dict, List, Any
+from fastapi import FastAPI, Response, HTTPException
 
-from fastapi import FastAPI, Response
+from .data import products
+from .schema import ProductSchema
 
 app = FastAPI()
-
-products: List[Dict[str, Any]] = [
-    {
-        "id": 1,
-        "name": "Laptop",
-        "description": "A high performance laptop",
-        "price": 999.99,
-        "available": True
-    },
-    {
-        "id": 2,
-        "name": "Smartphone",
-        "description": "A latest model smartphone",
-        "price": 699.99,
-        "available": True
-    },
-    {
-        "id": 3,
-        "name": "Headphones",
-        "description": "Noise-cancelling headphones",
-        "price": 199.99,
-        "available": False
-    },
-    {
-        "id": 4,
-        "name": "Monitor",
-        "description": "4K Ultra HD monitor",
-        "price": 299.99,
-        "available": True
-    },
-    {
-        "id": 5,
-        "name": "Keyboard",
-        "description": "Mechanical keyboard",
-        "price": 89.99,
-        "available": True
-    }
-]
 
 @app.get('/')
 def hello_world():
     return { "message": "hello world!" }
 
-@app.get('/products')
+@app.get('/products', response_model=list[ProductSchema])
 def list_products():
     return products
 
-@app.get('/products/{id}')
-def get_product_by_id(id: int, response: Response):
+@app.get('/products/{id}', response_model=ProductSchema)
+def get_product_by_id(id: int):
     # next is used to get an iterable, get the next value of an iterable or the default if not exists
     product = next((product for product in products if product["id"] == id), None)
     if product:
         return product
     
-    response.status_code = 404
-    return {"Message": "Product not Found."}
-    
+    raise HTTPException(status_code=404, detail="Product not found")
+
+@app.post('/products')
+def create_product(product: ProductSchema, response: Response):
+    products.append(product)
+
+    response.status_code = 201
+    return { "Message": "Product created successfully" }
